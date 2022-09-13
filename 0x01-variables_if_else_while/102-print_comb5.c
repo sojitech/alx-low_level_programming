3e961094c21a4a042f0fd20f5beae29551216bdc
Daniel Adesoji <sojitech@github.com>
2022-09-13 05:12:58 -0700

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -24,8 +24,8 @@ for(num2 = num1 + 1; num2 <= 99; num2++)
 putchar((num1 / 10) + 0);
 putchar((num1 % 10) + 0);
 putchar(' ');
-putchar((n2 / 10) + 0);
-putchar((n2 % 10) + 0);
+putchar((num2 / 10) + 0);
+putchar((num2 % 10) + 0);
 
 if(num1 != 98 && num2 == 99)
 		

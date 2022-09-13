8fe6b21dc3759050776d9a7c96102a9dced463d0
Daniel Adesoji <sojitech@github.com>
2022-09-13 03:18:32 -0700

    Software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
+*/
+int main()
+{
+int n1 = 0, n2;
+while (n1 <= 99)
+{
+	n2 = n1;
+	while(n2 <= 99)
+	{
+		if (n2 != n1)
+		{
+			putchar(n1 / 10) + 48)
+			putchar(n1 % 10) + 48)
+			putchar(' ');
+			putchar((n2 / 10) + 48);
+			putchar(n2 % 10) + 48);
+			
+			if(n1 != 98 || n2 != 99)
+			{
+				putchar(',');
+				putchar(' '); 
+			} 
+		}
+		++n2;
+	}
+	++n1;
+}
+putchar('\n');
+return 0;
+}

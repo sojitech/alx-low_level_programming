cbbf4e0c3b083d0e4cf45785acd015c431570427
Daniel Adesoji <sojitech@github.com>
2022-09-13 04:35:25 -0700

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,30 +9,29 @@
 */
 int main(void)
 {
-int n1 = 0, n2;
-while (n1 <= 99)
-{
-	n2 = n1;
-	while(n2 <= 99)
+	int n1 = 0, n2;
+	while (n1 <= 99)
 	{
-		if (n2 != n1)
+		n2 = n1;
+		while(n2 <= 99)
 		{
-			putchar((n1 / 10) + 48);
-			putchar((n1 % 10) + 48);
-			putchar(' ');
-			putchar((n2 / 10) + 48);
-			putchar((n2 % 10) + 48);
-			
-			if(n1 != 98 || n2 != 99)
+			if (n2 != n1)
 			{
-				putchar(',');
-				putchar(' '); 
-			} 
+				putchar((n1 / 10) + 48);
+				putchar((n1 % 10) + 48);
+				putchar(' ');
+				putchar((n2 / 10) + 48);
+				putchar((n2 % 10) + 48);
+				if(n1 != 98 || n2 != 99)
+				{
+					putchar(',');
+					putchar(' '); 
+				} 
+			}
+			++n2;
 		}
-		++n2;
+		++n1;
 	}
-	++n1;
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }

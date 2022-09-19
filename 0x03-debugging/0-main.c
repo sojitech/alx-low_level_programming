462c27ffe11676a68b54842c45a69b6f60aaf1d9
Daniel Adesoji <sojitech@github.com>
2022-09-18 22:40:53 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
-* mian - test function thta prints if integer is positive or negative
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 

aa91032ee364db5028384c896b887df14dda9a89
Daniel Adesoji <sojitech@github.com>
2022-09-25 15:10:35 -0700

    98 Battery st

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,21 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
-* Prototype files
-* import form here
+/**
+* void prototypes()
+* int prototypes()
 */
 
-int _putchar(char c);
 void reset_to_98(int *n);
-void swap_int)int *a, int *b);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-void put2(char *str);
+void rev_string(char *s);
+void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atol(char *s);
+int _atoi(char *str);
 
-#endif 
+#endif /*MAIN_H*/

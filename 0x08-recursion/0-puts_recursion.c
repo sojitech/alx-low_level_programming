c1351dd02f781a0b6bd132b354c8119e195bb187
Daniel Adesoji <sojitech@github.com>
2022-09-28 15:13:41 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,17 +4,17 @@
 * _puts_reursion - prints's a string followed by  new line
 * @s: string to be printed
 * 
-* Return: void
+* Return: void                                     
 */
-void _puts_recurse(char *s)
-
-{	if (*s == '0')
+void _puts_recursion(char *s)
+{	
+	if (*s == '0')
 	{
 		_putchar('\n')
 	}
 	esle
 	}
 		_putchar(s[0]);
-		_puts_recursion(s +1)
+		_puts_recursion(s + 1)
 	}
 }
